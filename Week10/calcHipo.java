53fc06d24a886a661a7ec3c427b39437cbc38f37
Luis Hernandez <la.hernandez.it@gmail.com>
2021-03-23 17:06:34 -0600

    Second commit

--- Week10/calcHipo.java
+++ Week10/calcHipo.java
@@ -1,15 +1,21 @@
+import java.io.*;
+
 public class calcHipo
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Ingresa el valor del primer cateto: ");
+        String cat1 = bf.readLine();
         System.out.print("Ingresa el valor del segundo cateto: ");
-        
-        printMessage("You fix the errors!");
+        String cat2 = bf.readLine();
+
+        printResult(cat1, cat2);
     }
 
-    public static void printMessage(String msg)
+    public static void printResult(String ladoA, String ladoB )
     {
-        System.out.println(msg);
+        double result = Math.sqrt(Math.pow(Integer.parseInt(ladoA), 2) + Math.pow(Integer.parseInt(ladoB), 2));
+        System.out.println("El valor de la hipotenusa para este triangulo es de: " + String.format("%.2f", result));
     }
 }
